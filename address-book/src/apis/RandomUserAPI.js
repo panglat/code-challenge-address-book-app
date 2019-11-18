0cf4ef9b7bcdc5ef0214ae9106232f2b6f4e6078
Pablo Anglat <pablo.anglat@globant.com>
2019-11-18 18:21:52 -0300

    Add RandomUserAPI

---
+++ address-book/src/apis/RandomUserAPI.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const RandomUserAPI = axios.create({
+  baseURL: 'https://randomuser.me/api/',
+});
+
+export const DEFAULT_GET_USER_LIST_RESULTS = 5;
+
+export const getUserList = async (
+  params = { results: DEFAULT_GET_USER_LIST_RESULTS }
+) => {
+  try {
+    const response = await RandomUserAPI.get('', {
+      params: {
+        results: params.results,
+      },
+    });
+    return response.data.results;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+export default RandomUserAPI;
