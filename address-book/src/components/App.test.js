7465cd23d828ccbb6d6c48b4bc68510b57eac8f9
Pablo Anglat <pablo.anglat@globant.com>
2019-11-18 19:58:03 -0300

    Fix test after redux

--- address-book/src/components/App.test.js
+++ address-book/src/components/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import createSagaMiddleware from 'redux-saga';
+import configureStore from '../store/configureStore';
+
+const sagaMiddleware = createSagaMiddleware();
+const mockStore = configureStore([sagaMiddleware]);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App store={mockStore} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- address-book/src/components/UserList/UserList.test.js
+++ address-book/src/components/UserList/UserList.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UserList from './UserList';
+import createSagaMiddleware from 'redux-saga';
+import configureStore from '../../store/configureStore';
+import { Provider } from 'react-redux';
+
+const sagaMiddleware = createSagaMiddleware();
+const mockStore = configureStore([sagaMiddleware]);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<UserList />, div);
+  ReactDOM.render(<Provider store={mockStore}><UserList /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
