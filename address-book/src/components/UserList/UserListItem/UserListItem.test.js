e5c7c45ed816e4a272d2176b5bb027ce1e0f7129
Pablo Anglat <pablo.anglat@globant.com>
2019-11-26 02:26:06 -0300

    Add UserListItem test

--- address-book/src/components/UserList/UserListItem/UserListItem.test.js
+++ address-book/src/components/UserList/UserListItem/UserListItem.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 import UserListItem from './UserListItem';
