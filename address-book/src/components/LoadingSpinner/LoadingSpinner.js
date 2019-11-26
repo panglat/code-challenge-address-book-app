c94b4c1a2e81d4ab8afff2403052196a9c5e0421
Pablo Anglat <pablo.anglat@globant.com>
2019-11-26 00:51:43 -0300

    Loading and fix fetch user

---
+++ address-book/src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './LoadingSpinner.scss';
+
+const LoadingSpinner = () => (
+  <div class="loading-spinner">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default LoadingSpinner;
--- address-book/src/components/UserList/UserList.js
+++ address-book/src/components/UserList/UserList.js
@@ -20,6 +20,7 @@ import {
   USERS_BATCH_SIZE,
   USERS_MAX_CATALOGUE_LENGTH,
 } from '../../utils/constants';
+import LoadingSpinner from '../LoadingSpinner/LoadingSpinner';
 
 const UserList = () => {
   const dispatch = useDispatch();
@@ -27,7 +28,9 @@ const UserList = () => {
   const isFetchingUsers = useSelector(state => loadingUsers(state));
   const fetchFailed = useSelector(state => loadUserFailed(state));
   const recordsToDisplay = useSelector(state => userRecordsToDisplay(state));
-  const userFilterLowerCase = useSelector(state => userSearch(state)).toLowerCase();
+  const userFilterLowerCase = useSelector(state =>
+    userSearch(state)
+  ).toLowerCase();
   const selectedNationalities = useSelector(state =>
     settingsNationalitySearch(state)
   );
@@ -39,7 +42,7 @@ const UserList = () => {
 
   useEffect(() => {
     if (!isFetchingUsers && users.length <= recordsToDisplay) {
-      if (recordsToDisplay < USERS_MAX_CATALOGUE_LENGTH) {
+      if (recordsToDisplay < USERS_MAX_CATALOGUE_LENGTH && !fetchFailed) {
         dispatch(
           requestUsers({
             results: USERS_BATCH_SIZE,
@@ -59,6 +62,7 @@ const UserList = () => {
     users.length,
     recordsToDisplay,
     selectedNationalities,
+    fetchFailed,
   ]);
 
   const onItemClick = user => {
@@ -69,7 +73,8 @@ const UserList = () => {
     const handleScroll = () => {
       if (
         window.innerHeight + document.documentElement.scrollTop !==
-        document.documentElement.offsetHeight && !isFetchingUsers
+          document.documentElement.offsetHeight &&
+        !isFetchingUsers
       )
         return;
       dispatch(usersRecordsToDisplay(users.length));
@@ -91,8 +96,8 @@ const UserList = () => {
               return false;
             }
             if (userFilterLowerCase) {
-              const {name} = user;
-              const fullNameLowercase =  `${name.first} ${name.last}`.toLowerCase();
+              const { name } = user;
+              const fullNameLowercase = `${name.first} ${name.last}`.toLowerCase();
               return fullNameLowercase.includes(userFilterLowerCase);
             }
             return true;
@@ -108,10 +113,15 @@ const UserList = () => {
             );
           })}
       {isFetchingUsers && users.length === recordsToDisplay && (
-        <div>Fetching users</div>
+        <div className="user-list__loading-message">
+          <LoadingSpinner />
+          <span>Fetching users</span>
+        </div>
       )}
-      {!isFetchingUsers && fetchFailed && <div>User fetch failed</div>}
-      {users.length >= USERS_MAX_CATALOGUE_LENGTH && <div>End of file</div>}
+      {!isFetchingUsers && fetchFailed && <div className="user-list__failed-message">User fetch failed</div>}
+      {users.length >= USERS_MAX_CATALOGUE_LENGTH && (
+        <div className="user-list__end-of-user-catalog-message">End of users catalog</div>
+      )}
       {selectedUser && (
         <UserDetailsModal
           onCloseModal={() => setSelectedUser(null)}
