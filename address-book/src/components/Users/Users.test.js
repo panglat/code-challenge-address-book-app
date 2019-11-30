12712b005e29c785032b01f7927edce7eb02f618
Pablo Anglat <pablo.anglat@globant.com>
2019-11-30 15:39:14 -0300

    fix test

--- address-book/src/components/Users/Users.test.js
+++ address-book/src/components/Users/Users.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import configureStore from '../../store/configureStore';
+
 import Users from './Users';
+
+const sagaMiddleware = createSagaMiddleware();
+const mockStore = configureStore([sagaMiddleware]);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Users />, div);
+  ReactDOM.render(<Provider store={mockStore}><Users /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
